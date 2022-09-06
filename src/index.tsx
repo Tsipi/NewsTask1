39dd47e92863088d6aeb41b3bcd2eb076e3d3552
darasus <idarase@gmail.com>
2022-09-06 21:39:39 +0200

    add prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
-import React from "react";
+import * as React from "react";
 // @ts-ignore
-import  { createRoot } from 'react-dom/client'; 
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { store } from "./store";
 import { Provider } from "react-redux";
 
-const root = createRoot(document.getElementById("root"));
+  const root = createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
